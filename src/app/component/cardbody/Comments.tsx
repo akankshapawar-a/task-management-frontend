479794721c6c08f3867ade2f72170cb36d751fdb
Akanksha Pawar <apawar@intraedge.com>
2025-10-02 14:09:55 +0530

    comment done

--- src/app/component/cardbody/Comments.tsx
+++ src/app/component/cardbody/Comments.tsx
@@ -1,17 +1,160 @@
- import React from 'react'
- import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
- import TipTapFullEditor from './TextArea';
- const Comments = () => {
-   return (
-     <div>
-         <div className='flex space-x-2'>
-        <ChatOutlinedIcon sx={{color:'#626f86'}}/>
-        <p className=' text-xl'>Comments</p>
+"use client";
+import React, { useEffect, useState } from "react";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import TipTapFullEditor from "./TextArea";
+import axios from "axios";
+import { SHOW_COMMENTS } from "@/app/Redux/CardsReducer";
+import { useDispatch, useSelector } from "react-redux";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { RootState } from "@/app/Redux/cards.Type";
+import moment from "moment";
+import {IconButton } from "@mui/material";
+import { stripHtml } from "@/app/utils/utils";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditIcon from "@mui/icons-material/Edit";
+interface CommentProps {
+  cardIds: string;
+}
+const Comments = ({ cardIds }: CommentProps) => {
+  const [showEditor, setShowEditior] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const comment = useSelector((state: RootState) => state.board.comment);
+  const ParticularComment = comment.filter(
+    (Comments) => Comments.cardId === cardIds
+  );
+  console.log("comment", comment);
+  const dispatch = useDispatch();
+
+  const handleGetComment = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:5000/api/card/getcomment"
+      );
+      if (response.data.status === "SUCCESS") {
+        const datauser = response.data.comments;
+        dispatch({ type: SHOW_COMMENTS, payload: datauser });
+        console.log("data comment", datauser);
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error saving comments:", error);
+    } finally {
+      // dispatch({ type: SET_LOADING, payload: false });
+    }
+  };
+
+  useEffect(() => {
+    handleGetComment();
+  }, []);
+
+  const handleComments = async (value: string) => {
+    const URL = "http://127.0.0.1:5000/api/";
+    const username = localStorage.getItem("username");
+    const payload = {
+      name: username,
+      cardId: cardIds,
+      comments: value,
+    };
+    try {
+      const response = await axios.post(`${URL}card/newcomment`, payload);
+
+      if (response.data.status !== "SUCCESS") {
+        console.error("Save failed:", response.data);
+        return;
+      }
+      handleGetComment();
+    } catch (error) {
+      console.error("Error saving description:", error);
+    }
+  };
+
+  const handleDeleteComment = async (id: string) => {
+    try {
+      const response = await axios.delete(
+        `http://127.0.0.1:5000/api/commentdelete/${id}`
+      );
+      if (response.data.status === "SUCCESS") {
+        handleGetComment();
+      }
+    } catch (error) {
+      console.error("Error saving comments:", error);
+    }
+  };
+  const handleCommentEdit = async (id: string, newText: string) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:5000/api/comment/edit/${id}`,
+        { comments: newText }
+      );
+      if (response.data.status === "SUCCESS") {
+        handleGetComment();
+      }
+    } catch (error) {
+      console.error("Error saving comments:", error);
+    }
+  };
+  return (
+    <div>
+      <div className="flex space-x-2">
+        <ChatOutlinedIcon sx={{ color: "#626f86" }} />
+        <p className=" text-xl mb-4">Comments</p>
       </div>
-     {/* <TipTapFullEditor cardId={''}/> */}
-     </div>
-   )
- }
- 
- export default Comments
- 
+      <TipTapFullEditor
+        type="comment"
+        initalValue=""
+        showEditor={showEditor}
+        setShowEditior={setShowEditior}
+        cardId={cardIds}
+        onSave={handleComments}
+      />
+      {ParticularComment.map((user, index) => (
+        <div key={index}>
+          <div className="inline-flex space-x-1.5 mt-4">
+            <AccountCircleIcon fontSize="large" />
+
+            <div className="space-x-2">
+              <span className="text-lg">{user.name}</span>
+              <span className=" text-xs text-blue-500">
+                {moment(user.date).format("MMM DD, YYYY, h:mm A")}
+              </span>
+            </div>
+          </div>
+          {!editMode ? (
+            <>
+              <div
+                dangerouslySetInnerHTML={{ __html: user.comments }}
+                className=" pl-7 rounded-lg p-1.5 border-0 border-gray-500 shadow-sm"
+              />
+              <div className="flex space-x-2 float-right">
+                <IconButton
+                  aria-label="delete"
+                  size="small"
+                  onClick={() => setEditMode(true)}
+                >
+                  <EditIcon fontSize="inherit" sx={{ color: "blue" }} />
+                </IconButton>
+                <IconButton
+                  aria-label="delete"
+                  size="small"
+                  onClick={() => handleDeleteComment(user._id)}
+                >
+                  <DeleteForeverIcon fontSize="inherit" sx={{ color: "red" }} />
+                </IconButton>
+              </div>
+            </>
+          ) : (
+            <TipTapFullEditor
+              type="comment"
+              initalValue={stripHtml(user.comments)}
+              showEditor={editMode}
+              setShowEditior={setEditMode}
+              onSave={(newText) => handleCommentEdit(user._id, newText)}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Comments;
--- src/app/component/cardbody/TextArea.tsx
+++ src/app/component/cardbody/TextArea.tsx
@@ -1,83 +1,113 @@
-"use client"
-import { useEffect, useState } from "react"
-import dynamic from "next/dynamic"
-import type { ReactQuillProps } from "react-quill-new"
-import "react-quill-new/dist/quill.snow.css"
-import { Box, Button } from "@mui/material"
-import axios from "axios"
-import { FETCH_ALL_CARDS_DATA } from "@/app/Redux/CardsReducer"
-import { useDispatch } from "react-redux"
+"use client";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import type { ReactQuillProps } from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import { Box, Button } from "@mui/material";
+import axios from "axios";
+import { FETCH_ALL_CARDS_DATA } from "@/app/Redux/CardsReducer";
+import { useDispatch } from "react-redux";
 
-const ReactQuill = dynamic<ReactQuillProps>(
-  () => import("react-quill-new"),
-  { ssr: false }
-)
+const ReactQuill = dynamic<ReactQuillProps>(() => import("react-quill-new"), {
+  ssr: false,
+});
 
-interface TextAreaProps{
-  cardId:string,
-  showEditor:boolean,
-  setShowEditior: React.Dispatch<React.SetStateAction<boolean>>; 
-  initalValue:string,
+interface TextAreaProps {
+  cardId?: string;
+  showEditor: boolean;
+  setShowEditior: React.Dispatch<React.SetStateAction<boolean>>;
+  initalValue: string;
+  type: "comment" | "description";
+  onSave?: (value: string) => void;
 }
-export default function TextArea({cardId,showEditor,setShowEditior,initalValue}:TextAreaProps) {
-  const [value, setValue] = useState<string>(initalValue||"");
-  const dispatch=useDispatch();
-  useEffect(()=>{
-    if(showEditor){
-     setValue(initalValue);
+export default function TextArea({
+  cardId,
+  showEditor,
+  setShowEditior,
+  initalValue,
+  type,
+  onSave,
+}: TextAreaProps) {
+  const [value, setValue] = useState<string>(initalValue || "");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (showEditor) {
+      setValue(initalValue);
     }
-  },[showEditor,initalValue])
-const handleDescription = async (value: string) => {
-  try {
-    const token = localStorage.getItem("token");
-    const response = await axios.put(
-      `http://127.0.0.1:5000/api/card/description/${cardId}`,
-      { description: value },
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
+  }, [showEditor, initalValue]);
 
-    if (response.data.status !== "SUCCESS") {
-      console.error("Save failed:", response.data);
+  const handleDescription = async (value: string) => {
+    const URL = "http://127.0.0.1:5000/api/";
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        `${URL}card/description/${cardId}`,
+        { description: value },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      if (response.data.status !== "SUCCESS") {
+        console.error("Save failed:", response.data);
+        return;
+      }
+      const boardResponse = await axios.get(`${URL}board`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (boardResponse.data.status === "SUCCESS") {
+        setValue("");
+        setShowEditior(false);
+        dispatch({
+          type: FETCH_ALL_CARDS_DATA,
+          payload: boardResponse.data.columns,
+        });
+      }
+    } catch (error) {
+      console.error("Error saving description:", error);
+    }
+  };
+
+  const handleSave = async () => {
+    if (onSave) {
+      await onSave(value);
+      setValue("");
+      setShowEditior(false);
       return;
     }
-    const boardResponse = await axios.get("http://127.0.0.1:5000/api/board", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-
-    if (boardResponse.data.status === "SUCCESS") {
-      setValue('');
-      setShowEditior(false);
-      dispatch({
-        type: FETCH_ALL_CARDS_DATA,
-        payload: boardResponse.data.columns,
-      });
+    if (type === "description") {
+      await handleDescription(value);
     }
-  } catch (error) {
-    console.error("Error saving description:", error);
-  }
-};
+  };
 
   return (
-  <div>
-    <div className="p-4 bg-gray-50 rounded-lg shadow-md">
-       {!showEditor && 
-       <input
-          type="text"
-          placeholder="Click to write..."
-          onFocus={() => setShowEditior(true)}
-          className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />}
-         {showEditor &&  <><ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        placeholder="Write your description here..."
-        className="min-h-[100px] text-base leading-relaxed p-4" />
-        <Box sx={{ display: 'flex', gap: '4px', marginTop: '0.7rem' }}>
-        <Button variant="contained"onClick={(e) => { e.preventDefault(); handleDescription(value);}}>Save</Button>
-          <Button onClick={() => setShowEditior(false)}>Cancel</Button>
-        </Box></>}
+    <div>
+      <div className="p-4 bg-gray-50 rounded-lg shadow-md">
+        {!showEditor && (
+          <input
+            type="text"
+            placeholder="Click to write..."
+            onFocus={() => setShowEditior(true)}
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+        )}
+        {showEditor && (
+          <>
+            <ReactQuill
+              theme="snow"
+              value={value}
+              onChange={setValue}
+              placeholder="Write your description here..."
+              className="min-h-[100px] text-base leading-relaxed p-4"
+            />
+            <Box sx={{ display: "flex", gap: "4px", marginTop: "0.7rem" }}>
+              <Button variant="contained" onClick={handleSave}>
+                Save
+              </Button>
+              <Button onClick={() => setShowEditior(false)}>Cancel</Button>
+            </Box>
+          </>
+        )}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
